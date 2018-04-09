0e6763efc818e605a49571b168e1a991d6edcda0
Huff <nikhuff@byui.edu>
2018-04-09 16:52:33 -0600

    added jquery to prevent bootstrap error

--- byui.js
+++ byui.js
@@ -1,6 +1,10 @@
 /*eslint-env node, browser, jquery*/
 
 /* Inject necessary scripts */
+var jquery = document.createElement('script');
+jquery.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js';
+document.body.appendChild(jquery);
+
 var bootStrap = document.createElement('script');
 bootStrap.src = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js';
 document.body.appendChild(bootStrap);
