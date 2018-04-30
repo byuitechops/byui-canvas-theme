20b956f48e5c7f8e082824dfe384e763224b6ac2
Danverde <gre12014@byui.edu>
2018-04-30 16:59:41 -0600

    removed courseBanner from online theme

--- online.js
+++ online.js
@@ -1,23 +1,6 @@
 /*eslint-env node, browser, jquery, es6*/
 
 document.addEventListener('DOMContentLoaded', () => {
-    /* Generate Course Banner at the top of the page */
-    try {
-        var courseNumber = document.location.pathname.split('/')[2];
-        /* If we know the course number And we're inside a course */
-        if (courseNumber && /\.com\/courses\/\d+/i.test(window.location.href)) {
-            $.getJSON('https://' + window.location.hostname + '/api/v1/courses/' + courseNumber, function (response) {
-                if (response.name && response.course_code) {
-                    var courseCode = response.course_code.toLowerCase().replace(/\s/g, '').split(':')[0];
-                    document.getElementById('wrapper').insertAdjacentHTML('afterbegin', `<div id='overallCourseBanner' class='${courseCode}'>${response.name}</div>`);
-                }
-            });
-        }
-    } catch (bannerErr) {
-        console.error(bannerErr);
-    }
-
-
     /* Insert copyright footer */
     try {
         var p = document.createElement('p');
--- production/online.js
+++ production/online.js
@@ -1,29 +1,17 @@
 /*eslint-env node, browser, jquery, es6*/
 
 document.addEventListener('DOMContentLoaded', function () {
-    /* Generate Course Banner at the top of the page */
-    try {
-        var courseNumber = document.location.pathname.split('/')[2];
-        /* If we know the course number And we're inside a course */
-        if (courseNumber && /\.com\/courses\/\d+/i.test(window.location.href)) {
-            $.getJSON('https://' + window.location.hostname + '/api/v1/courses/' + courseNumber, function (response) {
-                if (response.name && response.course_code) {
-                    var courseCode = response.course_code.toLowerCase().replace(/\s/g, '').split(':')[0];
-                    document.getElementById('wrapper').insertAdjacentHTML('afterbegin', '<div id=\'overallCourseBanner\' class=\'' + courseCode + '\'>' + response.name + '</div>');
-                }
-            });
-        }
-    } catch (bannerErr) {
-        console.error(bannerErr);
-    }
-
     /* Insert copyright footer */
     try {
         var p = document.createElement('p');
         p.innerHTML = 'Copyright ' + new Date().getFullYear() + ' Brigham Young University-Idaho';
         p.classList.add('copyright');
         var page = document.getElementById('content');
-        page.appendChild(p);
+        if (page) {
+            page.appendChild(p);
+        } else {
+            console.log('unable to add copyright footer to page');
+        }
     } catch (copyrightErr) {
         console.error(copyrightErr);
     }
