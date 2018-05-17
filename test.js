34c504ef2a5955817fded8f1eba7dd1d82b14d3e
Danverde <gre12014@byui.edu>
2018-05-17 13:17:23 -0600

    injects custom style tag for browsers without css3 variable support

--- test.js
+++ test.js
@@ -4,14 +4,21 @@ try {
     /* If we're inside a course */
     if (/\.com\/courses\/\d+/i.test(window.location.href)) {
         document.querySelector('.ic-app-nav-toggle-and-crumbs').id = 'courseBanner';
-        // document.getElementById('courseBanner').style.backgroundColor = 'green';
         
         /* get width of breadcrumbs */
         var breadcrumbWidth = document.getElementById('breadcrumbs').offsetWidth;
 
+        /* set background image of courseBanner */
         document.getElementById('courseBanner').style.backgroundImage = `url(https://byui.instructure.com/courses/${courseNumber}/file_contents/course%20files/template/courseBanner.jpg)`;
 
+        /* set width dynamically */
         document.querySelector('html').style.setProperty('--banner-fade-width', `calc(${breadcrumbWidth}px + 450px)`);
+
+        document.querySelector('head').insertAdjacentHTML('beforeend', `<style type='text/css'>
+        .ic-app-nav-toggle-and-crumbs::before {
+            width: calc(${breadcrumbWidth}px + 450px);
+        }
+        </style>`);
     }
 } catch (bannerErr) {
     console.error(bannerErr);
