c674505dc2cf7bf3aa297017e9839c1a9c747ab6
Levi Stum <levistum@gmail.com>
2019-06-13 16:46:13 -0600

    Dynamized it!

--- inlineFeedback/inline-feedback.js
+++ inlineFeedback/inline-feedback.js
@@ -2,6 +2,14 @@
 /* eslint no-console:0 */
 /* global tinyMCE, tippy, $ */
 
+const defaultBackgroundColor = "rgba(245, 245, 245)";
+
+const positiveClickColor = "rgba(0, 255, 0, 0.6)";
+const positiveHoverColor = "rgba(0, 255, 0, 0.2)";
+
+const negativeClickColor = "rgba(255, 0, 0, 0.6)";
+const negativeHoverColor = "rgba(255, 0, 0, 0.2)";
+
 (function () {
 
     function loadFeedback() {
@@ -10,79 +18,18 @@
         newDiv.setAttribute("id", "inlineFeedback");
         newDiv.style.textAlign = "center";
 
-        var thumbsUp = document.createElement("button");
-        thumbsUp.innerHTML = "&#128077;&#xFE0E;";
-        newDiv.appendChild(thumbsUp);
-
-        var thumbsDown = document.createElement("button");
-        thumbsDown.innerHTML = "&#128078;&#xFE0E;";
-        newDiv.appendChild(thumbsDown);
-
-        var lineBreak = document.createElement("br");
-
-        var data = {};
-
-        ratingButtons = newDiv.querySelectorAll("button");
-
-        ratingButtons.forEach((b) => {
-            b.style.border = "1px solid rgb(199, 205, 209)";
-            b.style.color = "#2D3B45";
-            b.style.borderRadius = "3px";
-            b.style.padding = "15px";
-            b.style.margin = "10px";
-            b.style.fontSize = "2em";
-            b.style.backgroundColor = "rgb(245, 245, 245)";
-            b.style.outline = "none";
-        });
-
         // Thumbs Up Logic
-        thumbsUp.addEventListener("mouseover", (event) => {
-            if (event.target.style.backgroundColor != "rgba(0, 255, 0, 0.6)") {
-                event.target.style.backgroundColor = "rgba(0, 255, 0, 0.2)"
-            }
-        });
-        thumbsUp.addEventListener("mouseout", (event) => {
-            if (event.target.style.backgroundColor != "rgba(0, 255, 0, 0.6)") {
-                event.target.style.backgroundColor = "rgba(245, 245, 245)"
-            }
-        });
-        thumbsUp.addEventListener("click", (event) => {
-            if (event.target.style.backgroundColor === "rgba(0, 255, 0, 0.6)") {
-                event.target.style.backgroundColor = "rgba(0, 255, 0, 0.2)"
-            } else event.target.style.backgroundColor = "rgba(0, 255, 0, 0.6)"
-            data.feedback = "Positive";
-            data.url = window.location;
-            data.userID = ENV.current_user.id;
-            data.contextAssetID = ENV.context_asset_string;
-            newDiv.appendChild(lineBreak);
-            createSurvey(newDiv, data);
-            thumbsDown.style.display = "none";
-        }, { once: true });
+        var thumbsUp = createButton("&#128077;&#xFE0E;");
+        addMouseEvents(thumbsUp, positiveClickColor, positiveHoverColor, defaultBackgroundColor);
+        addClickEvent(thumbsUp, newDiv, positiveClickColor, "Positive");
 
         // Thumbs Down Logic
-        thumbsDown.addEventListener("mouseover", (event) => {
-            if (event.target.style.backgroundColor != "rgba(255, 0, 0, 0.6)") {
-                event.target.style.backgroundColor = "rgba(255, 0, 0, 0.2)"
-            }
-        });
-        thumbsDown.addEventListener("mouseout", (event) => {
-            if (event.target.style.backgroundColor != "rgba(255, 0, 0, 0.6)") {
-                event.target.style.backgroundColor = "rgba(245, 245, 245)"
-            }
-        });
-        thumbsDown.addEventListener("click", (event) => {
-            if (event.target.style.backgroundColor === "rgba(255, 0, 0, 0.6)") {
-                event.target.style.backgroundColor = "rgba(255, 0, 0, 0.2)"
-            } else event.target.style.backgroundColor = "rgba(255, 0, 0, 0.6)"
-            data.feedback = "Negative";
-            data.url = window.location;
-            data.userID = ENV.current_user.id;
-            data.contextAssetID = ENV.context_asset_string;
-            newDiv.appendChild(lineBreak);
-            createSurvey(newDiv, data);
-            thumbsUp.style.display = "none";
-        }, { once: true });
+        var thumbsDown = createButton("&#128078;&#xFE0E;");
+        addMouseEvents(thumbsDown, negativeClickColor, negativeHoverColor, defaultBackgroundColor);
+        addClickEvent(thumbsDown, newDiv, negativeClickColor, "Negative");
 
+        newDiv.appendChild(thumbsUp);
+        newDiv.appendChild(thumbsDown);
         document.querySelector("#content").appendChild(newDiv);
     }
 
@@ -95,6 +42,52 @@
         surveyFrame.height = "400px";
         surveyFrame.style.borderWidth = 0;
         location.appendChild(surveyFrame);
+    }
+
+    function createButton(html) {
+        var button = document.createElement("button");
+        button.innerHTML = html;
+        button.style.border = "1px solid rgb(199, 205, 209)";
+        button.style.color = "#2D3B45";
+        button.style.borderRadius = "3px";
+        button.style.padding = "15px";
+        button.style.margin = "10px";
+        button.style.fontSize = "2em";
+        button.style.backgroundColor = defaultBackgroundColor;
+        button.style.outline = "none";
+        return button;
+    }
+
+    function addMouseEvents(obj, clickedColor, hoverColor, defaultColor) {
+        obj.addEventListener("mouseover", (event) => {
+            if (event.target.style.backgroundColor != clickedColor) {
+                event.target.style.backgroundColor = hoverColor
+            }
+        });
+        obj.addEventListener("mouseout", (event) => {
+            if (event.target.style.backgroundColor != clickedColor) {
+                event.target.style.backgroundColor = defaultColor
+            }
+        });
+    }
+
+    function addClickEvent(obj, div, color, feedback) {
+        var lineBreak = document.createElement("br");
+
+        obj.addEventListener("click", (event) => {
+            event.target.style.backgroundColor = color
+            var data = {
+                feedback: feedback,
+                url: window.location,
+                userID: ENV.current_user.id,
+                contextAssetID: ENV.context_asset_string
+            };
+            div.appendChild(lineBreak);
+            createSurvey(div, data);
+            // Loops through all the OTHER buttons and sets their dispay to "none"
+            Array.from(document.querySelector("#inlineFeedback").querySelectorAll("button")).filter(b => b != obj).map(b => b.style.display = "none");
+        }, { once: true });
+
     }
 
     loadFeedback();
